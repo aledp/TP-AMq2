7846d60c91d364119ae3416d91a7c042f0f72a50
aledp <aledp@users.noreply.github.com>
2023-10-18 08:34:21 -0300

    inference_pipeline.py ya se encuentra operativo.

--- src/inference_pipeline.py
+++ src/inference_pipeline.py
@@ -1,5 +1,21 @@
 import subprocess
 
-subprocess.run(['Python', 'feature_engineering.py'])
+subprocess.run(['python',
+                'feature_engineering.py',
+                '--input1',
+                'D:\\Github_aledp\\ceia_ApMaq2\\TP-AMq2\\data\\Train_BigMart.csv',
+                '--input2',
+                'D:\\Github_aledp\\ceia_ApMaq2\\TP-AMq2\\data\\Test_BigMart.csv',
+                '--output',
+                'D:\\Github_aledp\\ceia_ApMaq2\\TP-AMq2\\data\\FE_InferencePipe_processed_data.csv'],
+                check=True)
 
-subprocess.run(['Python', 'predict.py'])
+subprocess.run(['python', 
+                'predict.py', 
+                '-i',
+                'D:\\Github_aledp\\ceia_ApMaq2\\TP-AMq2\\data\\FE_InferencePipe_processed_data.csv',
+                '-m',
+                'D:\\Github_aledp\\ceia_ApMaq2\\TP-AMq2\\data\\modeloDump.pkl',
+                '-o',
+                'D:\\Github_aledp\\ceia_ApMaq2\\TP-AMq2\\data\\predicted_data.csv'],
+                 check=True)
